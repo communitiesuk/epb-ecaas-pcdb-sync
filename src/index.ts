0dcdf8a92ac7b666aaeaa8f5eac6d20a469a739b
James Wainwright <james.wainwright@madetech.com>
2026-04-08 12:36:38 +0100

    Target PCDB data from 2026-03-31

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { importProducts } from "./import_products_s3.js";
 
 export const handler = async () => {
 	await clearProducts();
-  	await importProducts("export_20260218.json");
+  	await importProducts("export_20260331.json");
 };
