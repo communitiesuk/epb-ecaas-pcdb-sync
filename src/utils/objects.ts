2bc29db3a77ffcebec963a9ec13cb052928ab397
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2026-05-05 11:41:25 +0100

    add oxlint for linting, and fix issue

--- src/utils/objects.ts
+++ src/utils/objects.ts
@@ -1,6 +1,6 @@
 import { toCamelCase } from "./strings.js";
 
-export const keysToCamelCase = (obj: Object): Record<string, unknown> => {
+export const keysToCamelCase = (obj: object): Record<string, unknown> => {
 	const output: Record<string, unknown> = {};
 
 	Object.keys(obj).forEach(prop => {
